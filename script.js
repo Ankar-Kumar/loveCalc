5d6f90d7bedf418fee72b478ecf3bbf37decb78b
Ankar_Kumar_Saha <64451539+losser09@users.noreply.github.com>
2022-04-28 07:25:33 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 
 
 function checkLove(){
-    let val=Math.floor(Math.random()*100)+1;
-    if(val+50<100) val=50+val;
-    document.querySelector('#love').value=val+"%";
+    const name=document.querySelector('#name').value;
+    const lname=document.querySelector('#lname').value;
+
+    if(name=="" || lname==""){
+        alert("please enter name")
+    }else if(name.length<=2 || lname.length<=2){
+        alert("enter atleast 3 charecter");
+    }else if((!isNaN(name)) || (!isNaN(lname))){
+        alert("number is not allowed");
+    }else{
+        let val=Math.floor(Math.random()*100)+1;
+        if(val+50<100) val=50+val;
+        document.querySelector('.love').innerHTML=`${val}%`;
+    }
+   
 }
